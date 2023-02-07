5d8fd8503776a593b400022d42e32dd6ff6c4cc2
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2023-02-07 19:23:17 -0400

    Modelos creados, tablas relacionadas

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,57 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Pokemon",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      hp: {
+        type: DataTypes.INTEGER,
+        defaultValue: 50,
+      },
+      attack: {
+        type: DataTypes.INTEGER,
+        defaultValue: 50,
+      },
+      defense: {
+        type: DataTypes.INTEGER,
+        defaultValue: 50,
+      },
+      speed: {
+        type: DataTypes.INTEGER,
+        defaultValue: 50,
+      },
+      heigth: {
+        type: DataTypes.INTEGER,
+        defaultValue: 40,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        defaultValue: 30,
+      },
+      image: {
+        type: DataTypes.TEXT,
+      },
+      dataBase: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
---
+++ api/src/models/Type.js
@@ -0,0 +1,19 @@
+const { DataTypes, UUIDV4 } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "Type",
+    {
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
