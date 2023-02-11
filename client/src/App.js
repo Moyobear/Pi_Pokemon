f5546758af38638c7521c6f2789405498cf88edf
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2023-02-10 22:40:43 -0400

    Estructura del Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import Home from "./views/Home/Home";
+import Landing from "./views/Landing/Landing";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
+    <div>
+      <Route exact path="/">
+        <Landing />
+      </Route>
+
+      <Route exact path="/home">
+        <Home />
+      </Route>
     </div>
   );
 }
