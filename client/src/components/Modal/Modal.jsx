b418b251ef3beaf931a96de2155268b6f9bed365
moyobear <rodriguezjmm@hotmail.com>
2023-03-05 11:22:39 -0400

    Errores varios

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import style from "./Modal.module.css";
+import { getAllPokemons, allNames } from "../../redux/actions";
 
 export default function Modal({ active, setActive, motivo }) {
   const history = useHistory();
+  const dispatch = useDispatch();
 
   function closeModal(active) {
     setActive(!active);
+    dispatch(getAllPokemons());
+    dispatch(allNames());
     history.push("/home");
   }
   return (
